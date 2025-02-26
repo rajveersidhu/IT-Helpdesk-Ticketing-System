f8b05e2c59eb4e0ba41b1fdc266f5dd8edbcad2c
Rajveer Sidhu <43861959+rajveersidhu@users.noreply.github.com>
2025-02-26 10:32:44 -0800

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/create-ticket">Create Ticket</Link>
+            <Link to="/login" onClick={() => localStorage.removeItem("token")}>Logout</Link>
+        </nav>
+    );
+};
+
+export default Navbar;
