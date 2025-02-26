2973904f3a0c9619ab8382e6c4a610e447cc8d6a
Rajveer Sidhu <43861959+rajveersidhu@users.noreply.github.com>
2025-02-26 10:16:36 -0800

    Add files via upload

--- src/main/java/com/helpdesk/repository/UserRepository.java
+++ src/main/java/com/helpdesk/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.helpdesk.repository;
 
-import com.helpdesk.model.Ticket;
+import com.helpdesk.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
