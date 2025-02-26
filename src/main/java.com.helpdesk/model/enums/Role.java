ed9b283ff939b93cef722a56a41745246f567c31
Rajveer Sidhu <43861959+rajveersidhu@users.noreply.github.com>
2025-02-26 10:18:54 -0800

    Delete src/main/java.com.helpdesk directory

--- src/main/java.com.helpdesk/model/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.helpdesk.model.enums;
-
-public enum Role {
-    USER, IT_STAFF, ADMIN
-}
--- src/main/java.com.helpdesk/model/enums/Status.java
+++
@@ -1,5 +0,0 @@
-package com.helpdesk.model.enums;
-
-public enum Status {
-    OPEN, IN_PROGRESS, CLOSED
-}
