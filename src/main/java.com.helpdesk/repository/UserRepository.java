a16eb90f78ad79401c2a82bb3e44f2055a84dace
Rajveer Sidhu <43861959+rajveersidhu@users.noreply.github.com>
2025-02-26 09:41:25 -0800

    Create UserRepository.java

---
+++ src/main/java.com.helpdesk/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.helpdesk.repository;
+
+import com.helpdesk.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
